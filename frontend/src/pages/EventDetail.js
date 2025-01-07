04eb3758f22e9e071f5b747826d86e1ee03ff7e8
Wushengyouhan <840835067@qq.com>
2025-01-07 12:09:18 +0800

    380. Controlling Which Data Should Be Deferred

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,19 +1,32 @@
-import { useRouteLoaderData, redirect } from "react-router-dom";
+import { Suspense } from "react";
+import { useRouteLoaderData, redirect, Await } from "react-router-dom";
 
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
 
 function EventDetailPage() {
-  const data = useRouteLoaderData("event-detail");
+  const { event, events } = useRouteLoaderData("event-detail");
 
-  return <EventItem event={data.event} />;
+  return (
+    <>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
+    </>
+  );
 }
 
 export default EventDetailPage;
 
-export async function loader({ request, params }) {
-  const id = params.eventId;
-
-  const response = await fetch(`http://localhost:8080/events/` + id);
+async function loadEvent(id) {
+  const response = await fetch("http://localhost:8080/events/" + id);
 
   if (!response.ok) {
     throw new Response(
@@ -23,8 +36,31 @@ export async function loader({ request, params }) {
       }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
   }
+}
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  return {
+    event: await loadEvent(id),
+    events: loadEvents(),
+  };
 }
 
 export async function action({ params, request }) {
@@ -38,6 +74,5 @@ export async function action({ params, request }) {
       status: 500,
     });
   }
-
   return redirect("/events");
 }
